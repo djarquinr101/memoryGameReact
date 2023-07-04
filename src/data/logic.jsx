108f881db3e3c3de27e3c13c19d3c8e808edba82
djarquinr101 <daniel.jarquin4.dj@gmail.com>
2023-07-04 09:48:39 -0600

    Function to pick up and move the boxes randomly was updated due to deployment issues

--- src/data/logic.jsx
+++ src/data/logic.jsx
@@ -1,19 +1,15 @@
 const gettingBoxes = (boxes, numberOfBoxes) => {
-    const shuffledBoxes = [...boxes]
-    let length = shuffledBoxes.length
-
-    while(length){
-        const randomIndex = Math.floor(Math.random() * length)
-        length --
-
-        [shuffledBoxes[length], shuffledBoxes[randomIndex]] = [
-            shuffledBoxes[randomIndex],
-            shuffledBoxes[length]
-          ];
-        }
-        return shuffledBoxes.slice(0, numberOfBoxes);
+    const shuffledBoxes = [...boxes];
+    let length = shuffledBoxes.length;
+  
+    while (length) {
+      const randomIndex = Math.floor(Math.random() * length);
+      [shuffledBoxes[length - 1], shuffledBoxes[randomIndex]] = [shuffledBoxes[randomIndex], shuffledBoxes[length - 1]];
+      length--;
     }
-
+  
+    return shuffledBoxes.slice(0, numberOfBoxes);
+  };
 
 
 const logic = {
